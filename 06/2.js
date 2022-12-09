81fb812e9c92b887884ced38ab07dd6cf56cb217
tgraveleau <teddy.graveleau@orange.fr>
2022-12-09 11:42:39 +0100

    ✨ Day 6 : speedrun 🚀

---
+++ 06/2.js
@@ -0,0 +1,18 @@
+import { fileToArray } from '../utils/file.js'
+const line = fileToArray('data.txt')[0].split('')
+
+const marker = []
+var found = false, position = 0
+
+while(!found) {
+    if (marker.length === 14) {
+        marker.shift(line[position])
+    }
+    marker.push(line[position])
+    position++
+    if (marker.length === 14 && !marker.find((m1, i1) => marker.find((m2, i2) => (i1 !== i2 && m1 === m2)))) {
+        found = true
+    }
+}
+
+console.log(position)
